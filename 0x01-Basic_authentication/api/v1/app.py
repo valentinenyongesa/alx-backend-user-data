aa3003ee496a75760899d62f3e5c977f32b49083
valentinenyongesa <valentinenyongesa94@gmail.com>
2024-04-17 08:00:07 +0000

    Error handler: Unauthorized

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,6 +7,7 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from typing import Literal
 
 
 app = Flask(__name__)
@@ -19,6 +20,12 @@ def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
+
+@app.errorhandler(401)
+def unauthorized(error) -> tuple[str, Literal[401]]:
+    """ Not found handler
+    """
+    return jsonify({"error": "unauthorized"}), 401
 
 
 if __name__ == "__main__":
