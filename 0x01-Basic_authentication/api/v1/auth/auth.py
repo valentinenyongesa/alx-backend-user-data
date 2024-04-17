bc9ecaaadead1e6e850fb4a014424543d34ad45c
valentinenyongesa <valentinenyongesa94@gmail.com>
2024-04-17 10:11:19 +0000

    Has routes that do not need authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -19,13 +19,13 @@ class Auth:
         """
         if path and not path.endswith('/'):
             path = path + '/'
-        #Returns True if path is None
+        """Returns True if path is None"""
         if not path or path not in excluded_paths:
             return True
-        #Returns True if excluded_paths is None or empty
+        """Returns True if excluded_paths is None or empty"""
         if not excluded_paths or excluded_paths == []:
             return True
-        #Returns False if path is in excluded_paths
+        """Returns False if path is in excluded_paths"""
         if path in excluded_paths:
             return False
         return False
@@ -54,4 +54,6 @@ if __name__ == '__main__':
     print(a.require_auth("/api/v1/status/", ["/api/v1/status/"]))
     print(a.require_auth("/api/v1/status", ["/api/v1/status/"]))
     print(a.require_auth("/api/v1/users", ["/api/v1/status/"]))
-    print(a.require_auth("/api/v1/users", ["/api/v1/status/", "/api/v1/stats"]))
+    print(
+          a.require_auth("/api/v1/users", ["/api/v1/status/", "/api/v1/stats"])
+          )
