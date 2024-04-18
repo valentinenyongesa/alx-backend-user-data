c32fe246fbdd99637e27986a25596394fc15c4db
valentinenyongesa <valentinenyongesa94@gmail.com>
2024-04-18 11:06:35 +0000

    Basic auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -20,6 +20,11 @@ if getenv('AUTH_TYPE') == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
 
+if getenv('AUTH_TYPE') == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+
+
 @app.before_request
 def before_request() -> Optional[str]:
     """
@@ -42,11 +47,13 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Not found"}), 404
 
+
 @app.errorhandler(401)
 def unauthorized(error) -> tuple[str, Literal[401]]:
     """ Not found handler
     """
     return jsonify({"error": "unauthorized"}), 401
+
 
 @app.errorhandler(403)
 def forbidden(error) -> tuple[str, Literal[403]]:
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    """
+    pass
